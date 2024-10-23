8c4095d69c5c52ddf8d8ea4f86fec9a91e6d513e
dpineda6988 <daniel.m.pineda@gmail.com>
2024-10-23 12:25:59 -0400

    Updated cache in script

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,17 @@ def run_query(query):
     rows_raw = query_job.result()
     # Convert to list of dicts. Required for st.cache_data to hash the return value.
     rows = [dict(row) for row in rows_raw]
-    return rows
+
+    # Load the query data into a DataFrame
+    df = pd.DataFrame(rows)
+
+    # Pivot the data into the desired format and reset the index
+    df_pivoted = df.pivot_table(index=['year','country_name','country_code'],columns='indicator_name',values='value')
+    final_df = df_pivoted.reset_index()
+
+    # Replace NaN values with '0'
+    final_df.fillna(0, inplace=True)
+    return final_df
 
 query = """
     SELECT year, country_name, country_code, indicator_name, value
@@ -86,20 +96,11 @@ query = """
     ORDER BY year DESC, country_name, indicator_name
 """
 
-rows = run_query(query)
+final_df = run_query(query)
 
-# Load the query data into a DataFrame
-df = pd.DataFrame(rows)
-
-# Pivot the data into the desired format and reset the index
-df_pivoted = df.pivot_table(index=['year','country_name','country_code'],columns='indicator_name',values='value')
-final_df = df_pivoted.reset_index()
-
-# Replace NaN values with '0'
-final_df.fillna(0, inplace=True)
 
 # Create a list of the different indicators for the drop down menu
-series_names = ['Fertility rate', 'GDP per capita (current US$)', ]
+series_names = ['Fertility rate, total (births per woman)', 'GDP per capita (current US$)', ]
 
 # Dropdown for indicator layers
 selected_metric = st.selectbox("Select Metric", series_names)
@@ -109,36 +110,38 @@ slider_year = st.slider('Select a year', 1960, 2019, 2019)
 
 current_df = final_df[final_df['year']==slider_year]
 
-# Build the map
-map = folium.Map(location=(0,0), zoom_start=2, tiles='cartodb positron', min_zoom=3)
-
 geojson_data = requests.get(
     "https://raw.githubusercontent.com/python-visualization/folium-example-data/main/world_countries.json"
 ).json()
 
+'''
+for feature in geojson_data['features']:
+    country_id = feature['id']
+    feature['properties']['Fertility Rate'] = 'Fertility Rate: ' + str(current_df.loc[current_df['country_code']==country_id, 'Fertility rate, total (births per woman)'].values[0] if country_id in list(current_df['country_code']) else 'N/A')
+    feature['properties']['GDP per capita (current US$)'] = 'GDP per capita (current US$): ' + str(current_df.loc[current_df['country_code']==country_id, 'GDP per capita (current US$)'].values[0] if country_id in list(current_df['country_code']) else 'N/A')
+'''
+# Build the map
+map = folium.Map(location=(30,0), zoom_start=2, tiles='cartodb positron', min_zoom=2)
+
 
 fertility_choro = folium.Choropleth(
     geo_data=geojson_data,
-    name="Fertility Rate (births per woman)",
+    name=selected_metric,
     data=current_df,
-    columns=['country_code','Fertility rate, total (births per woman)'],
+    columns=['country_code',selected_metric],
     key_on="feature.id",
     fill_color="YlOrBr",
     fill_opacity=0.7,
     line_opacity=0.2,
     highlight=True,
-    legend_name="Fertility Rate (births per woman)",
+    legend_name=selected_metric,
     nan_fill_color='white'
 ).add_to(map)
 
-for feature in fertility_choro.geojson.data['features']:
-    country_id = feature['id']
-    feature['properties']['Fertility Rate'] = 'Fertility Rate: ' + str(current_df.loc[current_df['country_code']==country_id, 'Fertility rate, total (births per woman)'].values[0] if country_id in list(current_df['country_code']) else 'N/A')
-    feature['properties']['GDP per capita (current US$)'] = 'GDP per capita (current US$): ' + str(current_df.loc[current_df['country_code']==country_id, 'GDP per capita (current US$)'].values[0] if country_id in list(current_df['country_code']) else 'N/A')
-
-
 fertility_choro.geojson.add_child(
-    folium.features.GeoJsonTooltip(['name', 'Fertility Rate','GDP per capita (current US$)'], labels=False)
+    folium.features.GeoJsonTooltip(['name'], labels=False)
 )
 
-st_map = st_folium(map,width=1500, height=800)
+
+
+st_map = st_folium(map,width=1050, height=800)
